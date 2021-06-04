72a629950900146981c1f10472fdca46a3a1bb5d
RudainaK <rkhalil828@gmail.com>
2021-06-04 17:07:07 -0400

    another attempt at fixing auth error

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ reddit = asyncpraw.Reddit(  # reddit authentication stuff
     client_id="r_id",
     client_secret="r_secret",
     password="passwd",
-    user_agent="<r_name:1.0>",
+    user_agent="r_name",
     username="usrname",
 )
 reddit.read_only = True
