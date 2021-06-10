0e51623e0da2371d22592bc19dfe5717baf12c0c
RudainaK <rkhalil828@gmail.com>
2021-06-10 16:59:50 -0400

    adding close command

--- main.py
+++ main.py
@@ -27,50 +27,49 @@ bot = commands.Bot(command_prefix="!")  # discord command prefix
 async def on_ready():
     print("Ready to find pc parts for hopefully not trash prices!")
 
-while True: 
-    @bot.command()
-    async def test(ctx):
-        await ctx.send("works")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("works")
 
 
-    @bot.command()
-    async def stop(ctx):
-        await bot.close()
-        break
+@bot.command()
+async def stop(ctx):
+    await bot.close() 
 
 
-    @bot.command()
-    async def top(ctx):
-        subreddit1 = await reddit.subreddit("bapcsalescanada")
-        async for post1 in subreddit1.new(limit=1):
-            print(post1.title)
-            await ctx.send("ha")
-            await ctx.send(post1.title)
+@bot.command()
+async def top(ctx):
+    subreddit1 = await reddit.subreddit("bapcsalescanada")
+    async for post1 in subreddit1.new(limit=1):
+        print(post1.title)
+        await ctx.send("ha")
+        await ctx.send(post1.title)
 
 
-    @tasks.loop(minutes=15)  # every 15 min do the following
-    async def gettopfewposts():
-        postList.clear()
-        subreddit = await reddit.subreddit("bapcsalescanada")
-        async for post in subreddit.new(limit=10):  # initial list
-            p = post.title
-            for i in range(len(myComponents)):
-                if p.find(myComponents[i]) >= 0:
-                    postList.append(post.title)
-                    # print(post.title)
+@tasks.loop(minutes=15)  # every 15 min do the following
+async def gettopfewposts():
+    postList.clear()
+    subreddit = await reddit.subreddit("bapcsalescanada")
+    async for post in subreddit.new(limit=10):  # initial list
+        p = post.title
+        for i in range(len(myComponents)):
+            if p.find(myComponents[i]) >= 0:
+                postList.append(post.title)
+                # print(post.title)
 
-        if len(postList) > 0:
-            finalList = list(dict.fromkeys(postList))  # gets rid of accidental duplicates
-            msgL = '\n'.join(finalList)
-            bot.wait_until_ready()
-            print("test, id: ", int(os.getenv("UPDATE_CHANNEL_ID")))
-            print("id before cast is of type: ", type(os.getenv("UPDATE_CHANNEL_ID")))
-            print("id before cast is of type: ", type(int(os.getenv("UPDATE_CHANNEL_ID"))))
-            update_channel = bot.get_channel(int(os.getenv("UPDATE_CHANNEL_ID")))
-            print("update channel is type: ", type(update_channel))
-            await update_channel.send(msgL)
+    if len(postList) > 0:
+        finalList = list(dict.fromkeys(postList))  # gets rid of accidental duplicates
+        msgL = '\n'.join(finalList)
+        bot.wait_until_ready()
+        print("test, id: ", int(os.getenv("UPDATE_CHANNEL_ID")))
+        print("id before cast is of type: ", type(os.getenv("UPDATE_CHANNEL_ID")))
+        print("id before cast is of type: ", type(int(os.getenv("UPDATE_CHANNEL_ID"))))
+        update_channel = bot.get_channel(int(os.getenv("UPDATE_CHANNEL_ID")))
+        print("update channel is type: ", type(update_channel))
+        await update_channel.send(msgL)
 
 
-    gettopfewposts.start()
+gettopfewposts.start()
 
-    bot.run(token)
+bot.run(token)
